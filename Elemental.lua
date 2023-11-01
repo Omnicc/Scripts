448e0dd06654fd90e0b286c371d70ad283ca971c
Omnicc <149558604+Omnicc@users.noreply.github.com>
2023-11-01 08:41:12 +0300

    Update Elemental.lua

--- Elemental.lua
+++ Elemental.lua
@@ -1,6 +1,7 @@
-game.CheckByfronAndHyperion.Destroy()
-print("Hyperion And Byfron Has Been Deleted From The Existence")
-CheckVersion.2.600.713.Change.Android.BypassErrorCode280 = True
-print("Your Game Is Now 32 Bits Lmfao")
-Workspace.Set.Executor.Codex = True
-print("The Exploit Was Successfully Applied")
+local Byfron = game:GetService("HyperionAntiCheat")
+if Byfron then
+    print("Deleting Byfron...")
+    Byfron = false
+else
+    print("You don't even have byfron dumbshit")
+end
